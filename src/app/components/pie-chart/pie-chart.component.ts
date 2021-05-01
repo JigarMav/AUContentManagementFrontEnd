b96c93072c25ae4037d8fdf7e605a8b037f82072
Jigar Mav <jbhanushali237@gmail.com>
2021-05-01 15:32:10 +0530

    Pie chart added

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,15 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import {
+  Label,
+  SingleDataSet,
+  monkeyPatchChartJsTooltip,
+  monkeyPatchChartJsLegend,
+} from 'ng2-charts';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.css']
+  styleUrls: ['./pie-chart.component.css'],
 })
 export class PieChartComponent implements OnInit {
+  public pieChartLabels: Label = ['Negative', 'Positive', 'Moderate'];
+  public pieChartData: SingleDataSet = [3, 70, 27];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
-  constructor() { }
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
 
-  ngOnInit(): void {
+  constructor(public http: HttpClient) {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
   }
-
+  ngOnInit(): void {}
 }
